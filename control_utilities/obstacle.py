f05d749544119723f5eb5806927716a266edda30
Aaron Young <aryoung5@wisc.edu>
2020-02-26 17:53:51 -0600

    Fixed small bug

--- control_utilities/obstacle.py
+++ control_utilities/obstacle.py
@@ -10,12 +10,19 @@ class Obstacle:
 
 class RandomObstacleGenerator:
     @staticmethod
-    def generateObstacles(path, n=1, dist=10, seed=0):
+    def generateObstacles(path, i_min, i_max, n=1, dist=10, seed=0, reversed=0):
+        if i_min > i_max:
+            raise Exception("i_min must be less than i_max")
+        elif i_max > len(path):
+            raise Exception("i_max must be less than the length of the path")
         obstacles = []
         for _ in range(n):
-            i = randint(0, len(path))
+            i = randint(i_min, i_max)
             if i+dist < len(path):
-                obstacles.append((i, Obstacle(path.points[i], path.points[i+dist])))
+                p1 = path.points[i]
+                p2 = path.points[i+dist]
             else:
-                obstacles.append((i, Obstacle(path.points[i], path.points[i+dist - len(path)])))
+                p1 = path.points[i]
+                p2 = path.points[i+dist - len(path)]
+            obstacles.append((i, Obstacle(p1, p2)))
         return dict(obstacles)
